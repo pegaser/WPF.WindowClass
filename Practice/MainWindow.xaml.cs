95695a0f83fd066e39ce727742e09f48ab1dd79c
pegaser <sperezgabriel@gmail.com>
2022-10-10 17:52:52 -0500

    10 OCTUBRE 2022

---
+++ Practice/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Rect bounds = Properties.Settings.Default.WindowPosition;
+                this.Top = bounds.Top;
+                this.Left = bounds.Left;
+                if (this.SizeToContent == SizeToContent.Manual)
+                {
+                    this.Width = bounds.Width;
+                    this.Height = bounds.Height;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No settings stored.");
+            }
+        }
+    }
+}
